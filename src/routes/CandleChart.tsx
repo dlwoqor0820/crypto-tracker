e83e1e2925c460f673d7e50ae425036424f7b546
dlwoqor0820 <dlwoqor0820@naver.com>
2024-08-25 22:37:21 +0900

    candle chart⬆️

--- src/routes/CandleChart.tsx
+++ src/routes/CandleChart.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchCoinHistory } from "./api";
 import ApexCharts from "react-apexcharts";
+import { useEffect, useState } from "react";
 
 interface IHistorical {
   time_open: string;
@@ -17,26 +18,38 @@ interface IChatPoprs {
   coinId: string;
 }
 
-interface IcandleData {
-  data: {
-    x: number;
-    y: number[];
-  };
+interface IcandlePosition {
+  x: Date;
+  y: number[];
 }
 
 function CandleChart({ coinId }: IChatPoprs) {
-  const { isLoading, data } = useQuery<IHistorical[]>({
+  const { isLoading, data: queryData } = useQuery<IHistorical[]>({
     queryKey: ["ohlcv", coinId],
     queryFn: () => fetchCoinHistory(coinId),
   });
+  const [posistionState, setPositionState] = useState<IcandlePosition[] | []>(
+    []
+  );
 
-  const mappedData = data?.map((item) => ({
-    x: item.time_open,
-    y: [item.open, item.high, item.low, item.close],
-  }));
+  useEffect(() => {
+    if (queryData) {
+      const mappedData = queryData?.map((item) => ({
+        x: new Date(item.time_open),
+        y: [item.open, item.high, item.low, item.close],
+      }));
+      if (mappedData !== undefined) {
+        setPositionState(mappedData);
+      } else {
+        setPositionState([]);
+      }
+    }
+  }, [queryData]);
 
+  /* 
   console.log(isLoading);
-  console.log(data);
+  console.log(mappedData); 
+  */
 
   return (
     <div>
@@ -45,15 +58,51 @@ function CandleChart({ coinId }: IChatPoprs) {
       ) : (
         <ApexCharts
           type="candlestick"
-          series={
-            [{ data: mappedData }]
-          }
+          series={[
+            {
+              name: "chart_1",
+              data: posistionState,
+            },
+          ]}
           options={{
+            theme: {
+              mode: "dark",
+            },
             chart: {
               height: 300,
               width: 1000,
               background: "transparent",
-            }
+              toolbar: {
+                show: false,
+              },
+            },
+            grid: {
+              show: false,
+            },
+            stroke: {
+              curve: "smooth",
+              width: 3,
+            },
+            xaxis: {
+              axisTicks: {
+                show: false,
+              },
+              axisBorder: {
+                show: false,
+              },
+              labels: {
+                show: false,
+              },
+            },
+            yaxis: {
+              show: false,
+            },
+            colors: ["#0fbcf9"],
+            tooltip: {
+              y: {
+                formatter: (value) => `${value.toFixed(2)} USD`,
+              },
+            },
           }}
         />
       )}
